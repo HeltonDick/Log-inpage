50b02f0a1ff921028281ffa93a66bfe55b7441ac
Helton Gabriel Dick <heltondick123@gmail.com>
2024-10-08 18:47:03 -0300

    pagina login

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log-in / Dick Picks</title>
+
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="log-in">
+        <label class="email">E-mail</label>
+        <input class="email" type="text" name="email">
+        <label class="senha">Senha</label>
+        <input class="senha" type="password" name="senha">
+        <button name="botao">Log-in</button>
+    </div>
+<script src="js/script.js"></script>
+</body>
+</html>
